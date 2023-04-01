621c7005daedb793261949a56930b31a6da45948
Andrew Alagna <aalagna04@gmail.com>
2023-04-01 17:30:29 -0400

    feat(nav): fix nav bar routing on /snake page

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,12 +1,7 @@
 import Snake from "react-simple-snake";
-import {useNavigate} from "react-router-dom"
 import {useEffect} from "react";
 
 const PlaySnake = () => {
-    // const [play, setPlay] = useState("false");
-    // const [button, showPlayButton] = useState("true");
-    const navigate = useNavigate();
-
     window.addEventListener("keydown", function(e) {
         if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
             e.preventDefault();
@@ -19,8 +14,7 @@ const PlaySnake = () => {
 
     return (
       <section id="snake">
-        <br />
-        <div className="overflow-hidden text-center h-full h-screen">
+        <div className="pt-10 overflow-hidden text-center h-full h-screen ">
           <h2 className="text-white text-3xl mb-1 font-medium mt-12">
             Use arrows or W/A/S/D keys to play:
           </h2>
@@ -30,15 +24,6 @@ const PlaySnake = () => {
             appleColor="red"
             snakeColor="green"
           />
-          <br />
-          <br />
-          <br />
-          <button
-            className="text-white bg-red-600 py-2 px-6 hover:bg-red-800 rounded text-lg mb-2"
-            onClick={() => navigate("/")}
-          >
-            Back to Portfolio
-          </button>
         </div>
       </section>
     );
